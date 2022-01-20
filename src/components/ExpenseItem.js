23d7555b7d329af6dda52d19e382bd00af06ca1d
alextay100 <alextaybnk@gmail.com>
2022-01-20 16:55:24 +0000

    added title state change

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,21 @@
+import React, { useState } from "react";
+
 import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const expenseTitle = props.title;
+  const [expenseTitle, setTitle] = useState(props.title);
+
   const expenseAmount = props.amount;
+
+  const clickUpdateTitle = () => {
+    if (expenseTitle === props.title) {
+      setTitle("Test");
+    } else {
+      setTitle(props.title);
+    }
+  };
 
   return (
     <Card className="expense-item">
@@ -13,6 +24,7 @@ function ExpenseItem(props) {
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">£{expenseAmount}</div>
       </div>
+      <button onClick={clickUpdateTitle}>Change Title</button>
     </Card>
   );
 }
